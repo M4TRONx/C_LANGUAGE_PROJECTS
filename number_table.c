508338187dbf5528ed3d53d670a9406db364becd
M4TRONx <hackermatronx9@gmail.com>
2022-08-05 16:57:55 +0600

    Mini Project

---
+++ number_table.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+
+int main(){
+
+
+
+    int a;
+
+    for(int a = 0;a==0;){
+        int n;
+    printf("Enter A Number : ");
+    scanf("%d",&n);
+    int a;
+    for(int i = 0;i<=10;i++){
+
+
+        a = n * i;
+
+    printf("%d * %d = %d\n",n,i,a);
+
+    }
+    }
+    return 0;
+}
