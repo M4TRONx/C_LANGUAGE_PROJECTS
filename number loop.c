abae233b94859c9f43f6908acae669dc7e96817d
M4TRONx <hackermatronx9@gmail.com>
2022-08-05 12:20:56 +0600

    mini project

---
+++ number loop.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main(){
+
+
+
+
+    int x;
+    printf("Enter a Number : ");
+    scanf("%d",&x);
+    int i = 0;
+
+    while(i<=x){
+        printf("%d\n",i);
+
+        i++;
+    }
+
+
+
+
+
+
+
+    return 0;
+}
