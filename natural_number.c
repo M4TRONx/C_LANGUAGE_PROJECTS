094500e57e2ce78dc6fbbe2a4cdd503936425239
M4TRONx <hackermatronx9@gmail.com>
2022-08-06 18:52:57 +0600

    repaired done

--- natural_number.c
+++ natural_number.c
@@ -2,19 +2,21 @@
 
 int main(){
 
+    int start,end;
+    printf("Enter start Number : ");
+    scanf("%d",&start);//input start number
+    printf("Enter End NUmber : ");
+    scanf("%d",&end);//input end number 
 
-    int x;
-    printf("Enter A Number : ");
-    scanf("%d",&x);
+    int main_value = 0;
 
-    int y = 0;
+    for(int worker=start;worker<=end;worker++){
+//START TO PLUS START NUMBER TO END NUMBER 
+        main_value = main_value +  worker;// main_value = NATURAL + NUMBER
 
-    for(int i=0;i<=x;i++){
-
-        y = y + i;
-
+    
     }
-    printf("%d\n%d",y);
+    printf("       +             \n%d (+NATURAL+) %d = %d",start,end,main_value);//PRINTED main_value(START + END )
 
     return 0;
 }
